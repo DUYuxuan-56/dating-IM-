954a29224c8c3937fabd545b227dedd6b3a2d9d8
unknown <d9du@uwaterloo.ca>
2020-08-03 09:17:09 -0400

    create graphql schema and set up apollo service

---
+++ schema/resolvers.ts
@@ -0,0 +1,13 @@
+import { DateTimeResolver, URLResolver } from 'graphql-scalars';
+import { chats } from '../db';
+
+const resolvers = {
+	Date:DateTimeResolver,
+	URL:URLResolver,
+	Query: {
+		chats() {
+	      return chats;
+	    },
+	},
+};
+export default resolvers;
